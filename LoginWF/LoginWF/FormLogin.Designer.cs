0daf0239ab1cbd47076c1909f32da91f5e86d34b
huanyd1 <65003197+huanyd1@users.noreply.github.com>
2020-12-31 08:34:39 +0700

    Add file local
    Add data

---
+++ LoginWF/LoginWF/FormLogin.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace LoginWF
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.picbUsernameOk = new System.Windows.Forms.PictureBox();
+            this.picbMinimine = new System.Windows.Forms.PictureBox();
+            this.picbClose = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnFacabook = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picbPasswordOk = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbUsernameOk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbMinimine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbPasswordOk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 753);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::LoginWF.Properties.Resources.login1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 753);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(815, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login into your account";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(731, 304);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(374, 30);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.Text = "Username";
+            this.txtUsername.MouseLeave += new System.EventHandler(this.txtUsername_MouseLeave);
+            this.txtUsername.MouseHover += new System.EventHandler(this.txtUsername_MouseHover);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(731, 375);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(374, 30);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.MouseLeave += new System.EventHandler(this.txtPassword_MouseLeave);
+            this.txtPassword.MouseHover += new System.EventHandler(this.txtPassword_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(960, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Forget Password?";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(731, 426);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 23);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Remember me";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Location = new System.Drawing.Point(731, 468);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(190, 49);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(879, 551);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "or login with";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(758, 679);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 19);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Don\'t have an account?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(921, 679);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 19);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Register here";
+            // 
+            // picbUsernameOk
+            // 
+            this.picbUsernameOk.Image = global::LoginWF.Properties.Resources.check;
+            this.picbUsernameOk.Location = new System.Drawing.Point(1111, 304);
+            this.picbUsernameOk.Name = "picbUsernameOk";
+            this.picbUsernameOk.Size = new System.Drawing.Size(37, 30);
+            this.picbUsernameOk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbUsernameOk.TabIndex = 18;
+            this.picbUsernameOk.TabStop = false;
+            this.picbUsernameOk.Visible = false;
+            // 
+            // picbMinimine
+            // 
+            this.picbMinimine.Image = global::LoginWF.Properties.Resources.minimize;
+            this.picbMinimine.Location = new System.Drawing.Point(1110, 1);
+            this.picbMinimine.Name = "picbMinimine";
+            this.picbMinimine.Size = new System.Drawing.Size(37, 30);
+            this.picbMinimine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbMinimine.TabIndex = 17;
+            this.picbMinimine.TabStop = false;
+            this.picbMinimine.Click += new System.EventHandler(this.picbMinimine_Click);
+            // 
+            // picbClose
+            // 
+            this.picbClose.Image = global::LoginWF.Properties.Resources.x_button;
+            this.picbClose.Location = new System.Drawing.Point(1144, 1);
+            this.picbClose.Name = "picbClose";
+            this.picbClose.Size = new System.Drawing.Size(37, 30);
+            this.picbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbClose.TabIndex = 16;
+            this.picbClose.TabStop = false;
+            this.picbClose.Click += new System.EventHandler(this.picbClose_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::LoginWF.Properties.Resources.twitter1;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(970, 592);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 45);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::LoginWF.Properties.Resources.google1;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(829, 592);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 45);
+            this.button2.TabIndex = 12;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnFacabook
+            // 
+            this.btnFacabook.BackColor = System.Drawing.Color.Transparent;
+            this.btnFacabook.BackgroundImage = global::LoginWF.Properties.Resources.facebook1;
+            this.btnFacabook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnFacabook.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFacabook.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(102)))), ((int)(((byte)(168)))));
+            this.btnFacabook.Location = new System.Drawing.Point(688, 592);
+            this.btnFacabook.Name = "btnFacabook";
+            this.btnFacabook.Size = new System.Drawing.Size(135, 45);
+            this.btnFacabook.TabIndex = 11;
+            this.btnFacabook.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LoginWF.Properties.Resources.password1;
+            this.pictureBox4.Location = new System.Drawing.Point(688, 375);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(37, 30);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LoginWF.Properties.Resources.username1;
+            this.pictureBox3.Location = new System.Drawing.Point(688, 304);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LoginWF.Properties.Resources.logo1;
+            this.pictureBox2.Location = new System.Drawing.Point(832, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(167, 47);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // picbPasswordOk
+            // 
+            this.picbPasswordOk.Image = global::LoginWF.Properties.Resources.check;
+            this.picbPasswordOk.Location = new System.Drawing.Point(1111, 375);
+            this.picbPasswordOk.Name = "picbPasswordOk";
+            this.picbPasswordOk.Size = new System.Drawing.Size(37, 30);
+            this.picbPasswordOk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbPasswordOk.TabIndex = 19;
+            this.picbPasswordOk.TabStop = false;
+            this.picbPasswordOk.Visible = false;
+            // 
+            // FormLogin
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.Appearance.BackColor = System.Drawing.Color.Black;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.picbPasswordOk);
+            this.Controls.Add(this.picbUsernameOk);
+            this.Controls.Add(this.picbMinimine);
+            this.Controls.Add(this.picbClose);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnFacabook);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogin";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbUsernameOk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbMinimine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbPasswordOk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnFacabook;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox picbClose;
+        private System.Windows.Forms.PictureBox picbMinimine;
+        private System.Windows.Forms.PictureBox picbUsernameOk;
+        private System.Windows.Forms.PictureBox picbPasswordOk;
+    }
+}
---
+++ LoginWF/LoginWF/FormLogin.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Runtime.InteropServices;
+
+
+namespace LoginWF
+{
+    public partial class FormLogin : DevExpress.XtraEditors.XtraForm
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        public FormLogin()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void txtUsername_MouseHover(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+                txtUsername.Text = "";
+            if(txtUsername.Text != "")
+            {
+                picbUsernameOk.Visible = true;
+            }
+            else
+            {
+                picbUsernameOk.Visible = false;
+            }
+
+        }
+
+        private void txtUsername_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "")
+            {
+                picbUsernameOk.Visible = true;
+            }
+            else
+            {
+                picbUsernameOk.Visible = false;
+            }
+        }
+
+        private void txtPassword_MouseHover(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+                txtPassword.Text = "";
+            txtPassword.UseSystemPasswordChar = true;
+
+            //Check Pw
+            if (txtPassword.Text != "")
+            {
+                picbPasswordOk.Visible = true;
+            }
+            else
+            {
+                picbPasswordOk.Visible = false;
+            }
+        }
+        private void txtPassword_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text != "")
+            {
+                picbPasswordOk.Visible = true;
+            }
+            else
+            {
+                picbPasswordOk.Visible = false;
+            }
+        }
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void picbClose_Click(object sender, EventArgs e)
+        {
+            DialogResult dlg = MessageBox.Show("Bạn có muốn thoát", "Câu hỏi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(dlg == DialogResult.Yes)
+            {
+                this.Close();
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void picbMinimine_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
